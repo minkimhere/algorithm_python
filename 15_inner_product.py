d49dfbc7b8dc178b6cf5998a5b92792d0630fecc
minkimhere <mj.kim.tech@gmail.com>
2022-05-13 00:08:28 +0900

    Update) 15_inner_product 다른 풀이 추가

--- 15_inner_product.py
+++ 15_inner_product.py
@@ -3,11 +3,19 @@
 # 이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
 
 # 푼 방법1
+# def solution(a, b):
+#     answer = 0
+#     for i in range(len(a)):
+#         answer += (a[i]*b[i])
+#     return answer
+
+# 푼 방법2
+# def solution(a, b):
+#     return sum(a[i]*b[i] for i in range(len(a)))
+
+# 다른 풀이 참고 방법3
 def solution(a, b):
-    answer = 0
-    for i in range(len(a)):
-        answer += (a[i]*b[i])
-    return answer
+    return sum([a*b for a, b in zip(a,b)])
 
 a = [1,2,3,4]
 b = [-3,-1,0,2]
