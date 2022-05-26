bdee169d68bba9943ff9c2f192037e092ab45f80
minkimhere <mj.kim.tech@gmail.com>
2022-05-26 23:54:14 +0900

    Update) 39_caesar_cipher_다른 방법 추가

--- 39_caesar_cipher.py
+++ 39_caesar_cipher.py
@@ -25,6 +25,18 @@ def solution(s, n):
     return answer
 
 
+# 방법2
+def solution(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
+        elif s[i].islower():
+            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+
+    return "".join(s)
+
+
 s = "AB"	
 n = 1
 # s = "z"	
